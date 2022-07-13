0009174758bdd671050589557d503e9816c7165e
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-13 22:11:23 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	int result = N * M - 1;
+
+	cout << result;
+	return 0;
+
+}
