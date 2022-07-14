fccd6c039c601eaccdaee772ac4006ff4e92fad5
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-15 00:35:06 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+	vector<int>V(9);
+	for (int i = 0; i < V.size(); i++) {
+		int num;
+		cin >> num;
+		V[i] = num;
+	}
+	int sum = 0;
+	for (int i = 0; i < V.size(); i++) {
+		sum += V[i];
+	}
+	int result = sum - 100;
+	int num1, num2;
+	for (int i = 0; i < V.size(); i++) {
+		for (int j = 1; j < V.size(); j++) {
+			if (V[i] + V[j] == result) {
+				num1 = V[i];
+				num2 = V[j];
+			}
+		}
+	}
+	for (int i = 0; i < V.size(); i++) {
+		if (V[i] != num1 && V[i] != num2) {
+			cout << V[i] << endl;
+		}
+	}
+	return 0;
+
+}
