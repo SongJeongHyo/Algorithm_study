beed49e3bd052d6e642775e61ecaf1662b900c59
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-17 01:06:51 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	cout << "172" << endl << "swjdgy";
+
+	return 0;
+ }
+
