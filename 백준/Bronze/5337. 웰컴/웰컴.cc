1f9de566c725a005bd0e60738bd728e934836324
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-17 01:03:22 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+	return 0;
+}
