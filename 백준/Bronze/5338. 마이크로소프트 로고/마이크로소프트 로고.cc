2be8830e0d163dc7cd30f72fe6d4983f9f9cf411
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-17 01:03:35 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	cout << "       _.-;;-._" << endl <<
+		"'-..-'|   ||   |" << endl <<
+		"'-..-'|_.-;;-._|" << endl <<
+		"'-..-'|   ||   |" << endl <<
+		"'-..-'|_.-''-._|" << endl;
+
+	return 0;
+ }
+
