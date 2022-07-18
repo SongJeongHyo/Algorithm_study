8f4b43aea41d5e03697ff7886de0b0f01a384e51
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-18 15:58:40 +0900

    [Bronze IV] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		cout << a + b << endl;
+	}
+
+
+	return 0;
+ }
+
