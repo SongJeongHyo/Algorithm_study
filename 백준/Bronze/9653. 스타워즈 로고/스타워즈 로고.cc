bb951b018519908dd073e936d09477e8128c58c4
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-18 16:03:14 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	cout << "    8888888888  888    88888" << endl <<
+		"   88     88   88 88   88  88" << endl <<
+		"    8888  88  88   88  88888" << endl <<
+		"       88 88 888888888 88   88" << endl <<
+		"88888888  88 88     88 88    888888" << endl << endl <<
+		"88  88  88   888    88888    888888" << endl <<
+		"88  88  88  88 88   88  88  88" << endl <<
+		"88 8888 88 88   88  88888    8888" << endl <<
+		" 888  888 888888888 88  88      88" << endl <<
+		"  88  88  88     88 88   88888888";
+
+	return 0;
+ }
+
