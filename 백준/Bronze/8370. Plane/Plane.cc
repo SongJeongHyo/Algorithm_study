a76069461dd62f669be82904f747afccde3a47c1
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-18 15:58:46 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	int result = a * b + c * d;
+
+	cout << result;
+
+	return 0;
+}
