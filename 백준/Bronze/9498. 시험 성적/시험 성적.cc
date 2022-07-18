d64539410e97d44ddc8ef45e475ed33f095466ca
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-18 16:02:56 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+
+	if (90 <=a  && a<= 100) {
+		cout << "A";
+	}
+	else if (80 <= a && a < 90) {
+		cout << "B";
+	}
+	else if (70 <= a && a< 80) {
+		cout << "C";
+	}
+	else if (60 <= a && a< 70) {
+		cout << "D";
+	}
+	else {
+		cout << "F";
+	}
+}
