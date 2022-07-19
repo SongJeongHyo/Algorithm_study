5686b4a0c3186f05f3a3095ba20e5a54a9aff813
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-20 00:45:19 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	/*ios::sync_with_stdio(false);
+	cin.tie(NULL);*/
+
+	cout << "         ,r'\"7" << endl << "r`-_   ,'  ,/" << endl
+		<< " \\. \". L_r\'" << endl << "   `~\\/" << endl
+		<< "      |" << endl << "      |";
+	
+	return 0;
+}
