6e6517b7a99158a4e4e47e9c78b85c43a5a67bf4
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-21 17:33:10 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄴ, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23803. 골뱅이 찍기 － ㄴ/골뱅이 찍기 － ㄴ.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < 4*n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "@";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 5*n; j++) {
+			cout << "@";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
