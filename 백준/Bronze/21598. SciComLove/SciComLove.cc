60948afa261c89e65f98341ab450ef88f30b3e46
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-23 15:16:30 +0900

    [Bronze IV] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	if (N == 1) {
+		cout << "SciComLove";
+	}
+	else if (N==2){
+		cout << "SciComLove" << endl << "SciComLove";
+	}
+	else {
+
+	}
+
+	return 0;
+ }
+
