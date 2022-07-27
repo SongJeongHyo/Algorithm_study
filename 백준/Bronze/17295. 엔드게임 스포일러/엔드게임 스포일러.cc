e148fbfbcf06e70aa57a6c5017e7ce6d473c7a4b
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-27 15:56:40 +0900

    [Bronze V] Title: 엔드게임 스포일러, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17295. 엔드게임 스포일러/엔드게임 스포일러.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	cout << "Avengers: Endgame";
+
+	return 0;
+}
