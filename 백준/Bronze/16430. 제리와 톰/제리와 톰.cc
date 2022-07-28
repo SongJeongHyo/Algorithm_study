4c9bda29aad93bc9fa2366c892c1cbad86e2918f
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-07-28 17:28:27 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int result = 0;
+	result = b - a;
+
+	cout << result << " " << b;
+	return 0;
+}
