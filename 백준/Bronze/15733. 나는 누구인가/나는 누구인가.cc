c108e54a7b14eab2d2bb94220039a8bcd1ed280f
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-08-03 00:51:19 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	cout << "I'm Sexy";
+	
+
+	return 0;
+}
