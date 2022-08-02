9ee1adacb98c7254f157eb7f9f4341910d3c953d
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-08-02 17:30:53 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	cout << "파이팅!!";
+
+	return 0;
+}
