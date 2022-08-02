29f3fc7abfaa8f7a6c3d4890537a1c84da7919f9
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-08-02 17:30:41 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	long long N, M;
+	cin >> N >> M;
+
+	if (N == M) {
+		cout << "1";
+	}
+	else {
+		cout << "0";
+	}
+
+	return 0;
+}
