10714f6434163425fe0f87ad758aed41550a2233
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-08-03 18:50:44 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	if (N == 0) {
+		cout << "YONSEI";
+	}
+	else {
+		cout << "Leading the Way to the Future";
+	}
+
+	return 0;
+ }
+
