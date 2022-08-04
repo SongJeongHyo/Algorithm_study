0b4b66c5f286489d520bc329ae0ee25e07ff979d
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-08-04 22:40:35 +0900

    [Bronze IV] Title: Vera and Outfits, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. Vera and Outfits/Vera and Outfits.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	cout << N * (N - 1);
+	
+	return 0;
+}
