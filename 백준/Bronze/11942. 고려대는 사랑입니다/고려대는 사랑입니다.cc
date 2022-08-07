c02d9d0d2d1b0b325db206cc2159135c29fbf8c5
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-08-08 00:38:42 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	cout << "고려대학교";
+
+	return 0;
+ }
+
