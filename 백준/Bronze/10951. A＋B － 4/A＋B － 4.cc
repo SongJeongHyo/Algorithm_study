04b4cde923ed97e6e06e012507c4cca435a18ba5
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-09-24 14:20:17 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int A,B;
+	
+	while (cin >> A >> B) {
+		
+		
+		cout << A + B << '\n';
+	}
+	
+	return 0;
+}
