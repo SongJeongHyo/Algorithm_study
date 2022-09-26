d6ce78f443cd1b24ddf75e105d405651a018cd94
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2022-09-26 17:53:12 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <stack>
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+
+	cout << str << "??!";
+
+	return 0;
+ }
+
