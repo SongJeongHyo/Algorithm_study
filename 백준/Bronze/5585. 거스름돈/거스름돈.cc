7c2495d0bf23ab073df70ca11dda487ef5f4031c
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2023-02-03 17:21:29 +0900

    [Bronze II] Title: 거스름돈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.cc
+++ 백준/Bronze/5585. 거스름돈/거스름돈.cc
@@ -26,65 +26,46 @@ int main() {
 	cin >> m;
 	change = 1000 - m;
 
-	int a = change / 500;
-	int b = change - 500;
-	
-	int c = b / 100;
-	int c1 = change / 100;
-	int d = b - 100 * c;
-	int d1 = change - 100 * c1;
-	int e = d / 50;
-	int e1 = d1 / 50;
-	int f = d - 50 * e;
-	int f1 = d1 - 50 * e1;
-	int g = f / 10;
-	int g1 = f1 / 10;
-	int h = f - 10 * g;
-	int h1 = f1 - 10 * g1;
-	int i = h / 5;
-	int i1 = h1 / 5;
-	int j = h - 5 * i;
-	int j1 = h1 - 5 * i1;
-	int k = j / 1;
-	int k1 = j1 / 1;
-
-	if (a >= 1) {
+	if (change >= 500) {
 		cnt++;
-		if (c >= 1) {
-			cnt += c;
-		}
-		if (d >= 1) {
-			cnt += e;
-		}
-		if (g >= 1) {
-			cnt += g;
-		}
-		if (i >= 1) {
-			cnt += i;
-		}
-		if (k >= 1) {
-			cnt += k;
+		change -= 500;
+	}
+
+	if (change >= 100) {
+		while (change >= 100)
+		{
+			cnt++;
+			change -= 100;
 		}
 	}
-	else {
-		if (c1 >= 1) {
-			cnt += c1;
-		}
-		if (d1 >= 1) {
-			cnt += e1;
-		}
-		if (g1 >= 1) {
-			cnt += g1;
-		}
-		if (i1 >= 1) {
-			cnt += i1;
-		}
-		if (k1 >= 1) {
-			cnt += k1;
-		}
-		
+
+	if (change >= 50) {
+		cnt++;
+		change -= 50;
 	}
-	cout << cnt;
+
+	if (change >= 10) {
+		while (change >= 10)
+		{
+			cnt++;
+			change -= 10;
+		}
+	}
 	
+	if (change >= 5) {
+		cnt++;
+		change -= 5;
+	}
+
+	if (change >= 1) {
+		while (change >= 1)
+		{
+			cnt++;
+			change -= 1;
+		}
+	}
+
+	cout << cnt;
+
 	return 0;
 }
