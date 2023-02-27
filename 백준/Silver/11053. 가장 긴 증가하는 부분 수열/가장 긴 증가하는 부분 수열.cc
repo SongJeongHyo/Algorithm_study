cee26b38308e7c598b57317e050a06b7908bc138
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2023-02-27 16:35:49 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <tuple>
+#include <stdint.h>
+#include <bitset>
+
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	int res = 0;
+	cin >> N;
+
+	vector<int>V(N);
+	vector<int>dp(1001);
+
+	for (int i = 0; i < V.size(); i++) {
+		cin >> V[i];
+	}
+
+	for (int i = 0; i < N; i++){
+		dp[i] = 1;
+
+		for (int j = 0; j < i; j++) {
+			if (V[i] > V[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		res = max(res, dp[i]);
+	}
+
+	cout << res;
+
+	return 0;
+}
