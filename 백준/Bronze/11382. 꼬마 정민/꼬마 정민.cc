080c04c1935624f2a7b56acbe47883c6e77675df
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2023-02-27 14:59:32 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <tuple>
+#include <stdint.h>
+#include <bitset>
+
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	long long A, B, C;
+
+	cin >> A >> B >> C;
+
+	cout << A + B + C;
+
+	return 0;
+}
