e272223a956d17dfc1e13433696450e7f58dc366
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2023-03-19 15:22:06 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+
+    sort(citations.begin(), citations.end(), greater<>());
+
+    for(int i = 0; i<citations.size(); i++){
+        if(i+1 <= citations[i]){
+            answer = i + 1;
+        }
+    }
+    return answer;
+}
