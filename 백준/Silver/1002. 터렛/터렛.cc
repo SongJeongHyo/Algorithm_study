4393ea79957acc1a7ffc6d5ccce7b66c2bc9490e
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2023-06-25 16:09:58 +0900

    [Silver III] Title: 터렛, Time: 8 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.cc
+++ 백준/Silver/1002. 터렛/터렛.cc
@@ -9,35 +9,41 @@
 #include <stack>
 #include <deque>
 #include <tuple>
+#include <stdint.h>
+#include <bitset>
+#include <set>
 
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
+	cout.tie(NULL);
+
 	int T;
 	cin >> T;
 
 	for (int i = 0; i < T; i++) {
-		double x1, y1, r1, x2, y2, r2 ;
+		double x1, y1, r1, x2, y2, r2;
 		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
 
 		double distance = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
 
-		if (distance < abs(r1 - r2) || distance > r1 + r2) {
-			cout << 0 << endl;
-		}
-		else if (x1 == x2 && y1 == y2 && r1 == r2) {
+		
+		if (x1 == x2 && y1 == y2 && r1 == r2) {
 			cout << -1 << endl;
 		}
 		else if (distance == r1 + r2 || distance == abs(r1 - r2)) {
 			cout << 1 << endl;
 		}
-		else if (abs(r1 - r2) < distance < r1 + r2) {
+		else if (abs(r1 - r2) < distance && distance < (r1 + r2)) {
 			cout << 2 << endl;
 		}
-		
+		else {
+			cout << 0 << endl;
+		}
+
 	}
+
 	return 0;
 }
