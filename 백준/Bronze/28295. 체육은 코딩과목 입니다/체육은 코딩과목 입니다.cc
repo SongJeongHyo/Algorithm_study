68534c5afb33a1a798ad16f2e3859d72511351b2
SongJeongHyo <79832986+SongJeongHyo@users.noreply.github.com>
2023-07-02 16:52:15 +0900

    [Bronze IV] Title: 체육은 코딩과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.cc
+++ 백준/Bronze/28295. 체육은 코딩과목 입니다/체육은 코딩과목 입니다.cc
@@ -1,34 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <sstream>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <tuple>
+#include <stdint.h>
+#include <bitset>
+#include <set>
+
 using namespace std;
 
 int main() {
-    int directions[10]; 
-    
-    for (int i = 0; i < 10; i++) {
-        cin >> directions[i];
-    }
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int currentDirection = 0; 
-    
-    for (int i = 0; i < 10; i++) {
-        if (directions[i] == 1) { 
-            currentDirection = (currentDirection + 1) % 4;
-        } else if (directions[i] == 2) { 
-            currentDirection = (currentDirection + 2) % 4;
-        } else if (directions[i] == 3) {
-            currentDirection = (currentDirection + 3) % 4; 
-        }
-    }
+	int res = 0;
 
-    if (currentDirection == 0) {
-        cout << "N";
-    } else if (currentDirection == 1) {
-        cout << "E";
-    } else if (currentDirection == 2) {
-        cout << "S";
-    } else if (currentDirection == 3) {
-        cout << "W";
-    }
+	for (int i = 0; i < 10; i++) {
+		int num;
+		cin >> num;
+		if (num == 1) {
+			res = (res + 1) % 4;
+		}
+		else if (num == 2) {
+			res = (res + 2) % 4;
+		}
+		else {
+			res = (res + 3) % 4;
+		}
 
-    return 0;
+		//cout << res << endl;
+	}
+
+	if (res == 0) {
+		cout << "N";
+	}
+	else if (res == 1) {
+		cout << "E";
+	}
+	else if (res == 2) {
+		cout << "S";
+	}
+	else {
+		cout << "W";
+	}
+
+	return 0;
 }
